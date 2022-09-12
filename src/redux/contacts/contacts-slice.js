40139c8cd65af186f2a6751ad612ba10b63c2bc6
Andrey Ivanov <prioritet2503@gmail.com>
2022-09-13 00:58:15 +0300

    save

--- src/redux/contacts/contacts-slice.js
+++ src/redux/contacts/contacts-slice.js
@@ -54,28 +54,3 @@ const contactsSlice = createSlice({
 });
 
 export default contactsSlice;
-
-// const contactsSlice = createSlice({
-//   name: 'contacts',
-//   initialState: [],
-//   reducers: {
-//     addContact: {
-//       reducer: (store, { payload }) => {
-//         return [...store, payload];
-//       },
-//       prepare: data => {
-//         return {
-//           payload: {
-//             ...data,
-//             id: shortid(),
-//           },
-//         };
-//       },
-//     },
-//     removeContact: (store, { payload }) =>
-//       store.filter(({ id }) => id !== payload),
-//   },
-// });
-// export const { addContact, removeContact } = contactsSlice.actions;
-
-// export default contactsSlice.reducer;
